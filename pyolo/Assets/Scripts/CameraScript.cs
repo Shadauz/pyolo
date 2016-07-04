20f206053d8ed67797bfcdebf4ae1f1d5e7cb800
Shadauz <shadauzzz@gmail.com>
2016-07-04 14:19:38 +0200

    0.0.5

---
+++ pyolo/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	void Awake () {
+		int scaling = 1;
+		int TargetHeight = Screen.height;
+		int PPA = 12;
+		if (TargetHeight < 700){
+			scaling = 1;
+		}
+		if (TargetHeight >= 700 && TargetHeight <= 900) {
+			scaling = 2;
+		}
+		if (TargetHeight >= 900) {
+			scaling = 3;
+		}
+		Camera.main.orthographicSize = TargetHeight/2/PPA/scaling;
+	}
+
+}
